ba86b01f53aa9f836d893b465d2319a3213c83a6
Nikolas Skoufis <nskoufis@seek.com.au>
2020-08-17 17:51:58 +1000

    Formatting

--- pkg/fns/token_replacer.go
+++ pkg/fns/token_replacer.go
@@ -1,68 +1,66 @@
 package fns
 
 import (
-  "sigs.k8s.io/kustomize/kyaml/yaml"
-  "strings"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
+	"strings"
 )
 
 const (
-  // Label used to enable token replacement
-  tokenReplaceEnabledLabelKey = "kpt.seek.com/token-replace"
+	// Label used to enable token replacement
+	tokenReplaceEnabledLabelKey = "kpt.seek.com/token-replace"
 
-  // Label value used to enable token replacement
-  tokenReplaceEnabledLabelValue = "enabled"
+	// Label value used to enable token replacement
+	tokenReplaceEnabledLabelValue = "enabled"
 )
 
 type TokenReplaceConfig struct {
-  Spec Spec `yaml:"spec,omitempty"`
+	Spec Spec `yaml:"spec,omitempty"`
 }
 
 type Spec struct {
-  Replacements []Replacement `yaml:"replacements,omitempty"`
+	Replacements []Replacement `yaml:"replacements,omitempty"`
 }
 
 type Replacement struct {
-  Token string `yaml:"token,omitempty"`
-  Value string `yaml:"value,omitempty"`
+	Token string `yaml:"token,omitempty"`
+	Value string `yaml:"value,omitempty"`
 }
 
-
 type TokenReplacer struct {
-  Config *TokenReplaceConfig
+	Config *TokenReplaceConfig
 }
 
 func (tr *TokenReplacer) Filter(rn *yaml.RNode) (*yaml.RNode, error) {
-  meta, err := rn.GetMeta()
-  if err != nil {
-    return nil, err
-  }
+	meta, err := rn.GetMeta()
+	if err != nil {
+		return nil, err
+	}
 
-  if meta.Annotations[tokenReplaceEnabledLabelKey] != tokenReplaceEnabledLabelValue {
-    return rn, nil
-  }
+	if meta.Annotations[tokenReplaceEnabledLabelKey] != tokenReplaceEnabledLabelValue {
+		return rn, nil
+	}
 
-  // We only perform token replacement on ConfigMaps for now
-  if meta.Kind == "ConfigMap" {
-    return rn, tr.processConfigMap(rn)
-  }
+	// We only perform token replacement on ConfigMaps for now
+	if meta.Kind == "ConfigMap" {
+		return rn, tr.processConfigMap(rn)
+	}
 
-  return rn, nil
+	return rn, nil
 }
 
 func (tr *TokenReplacer) processConfigMap(rn *yaml.RNode) error {
-  data := rn.Field("data")
-  if data == nil {
-    return nil
-  }
+	data := rn.Field("data")
+	if data == nil {
+		return nil
+	}
 
-  return data.Value.VisitFields(func(node *yaml.MapNode) error {
-    for _, r := range tr.Config.Spec.Replacements {
-      yn := node.Value.YNode()
-      if yn.Kind == yaml.ScalarNode {
-        yn.Value = strings.ReplaceAll(yn.Value, r.Token, r.Value)
-      }
-    }
-    return nil
-  })
+	return data.Value.VisitFields(func(node *yaml.MapNode) error {
+		for _, r := range tr.Config.Spec.Replacements {
+			yn := node.Value.YNode()
+			if yn.Kind == yaml.ScalarNode {
+				yn.Value = strings.ReplaceAll(yn.Value, r.Token, r.Value)
+			}
+		}
+		return nil
+	})
 }
-
