6f51bdd49d922364af1260fcd4caa5f173346901
Nikolas Skoufis <nskoufis@seek.com.au>
2020-08-18 11:31:02 +1000

    Newlines

--- pkg/fns/hash_dependency.go
+++ pkg/fns/hash_dependency.go
@@ -31,7 +31,7 @@ func (dh *DependencyHasher) Filter(rn *yaml.RNode) (*yaml.RNode, error) {
 
 	for k, v := range meta.Annotations {
 		if strings.HasPrefix(k, hashDependencyAnnotationPrefix) {
-      fmt.Fprintf(os.Stderr, "hashing dependency for %s/%s", meta.Namespace, meta.Name)
+      fmt.Fprintf(os.Stderr, "hashing dependency for %s/%s\n", meta.Namespace, meta.Name)
 			err := dh.hashDependency(rn, meta.Namespace, v)
 			if err != nil {
 				return rn, err
@@ -58,7 +58,7 @@ func (dh *DependencyHasher) Filter(rn *yaml.RNode) (*yaml.RNode, error) {
     }
     for k, v := range podMeta.Annotations {
       if strings.HasPrefix(k, hashDependencyAnnotationPrefix) {
-        fmt.Fprintf(os.Stderr, "hashing pod dependency for %s/%s", meta.Namespace, meta.Name)
+        fmt.Fprintf(os.Stderr, "hashing pod dependency for %s/%s\n", meta.Namespace, meta.Name)
         err := dh.hashDependency(podSpec, meta.Namespace, v)
         if err != nil {
           return rn, err
