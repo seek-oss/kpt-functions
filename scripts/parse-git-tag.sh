e704da4be66adc414b827403c1ede9335f1c7136
Nikolas Skoufis <nskoufis@seek.com.au>
2021-04-26 12:28:59 +1000

    Update workflow to only publish one function at a time
    This updates the Github actions workflow to only publish one function at
    a time.
    
    On master, we still publish all functions with the tag latest.
    
    However if the tag begins with a v, we are expecting a tag of the format
    `v1.2.3/function-name`. This tag is parsed, and then used to only
    publish the tagged function.

---
+++ scripts/parse-git-tag.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+usage() {
+  cat << EOF
+Usage: $(basename "${0}") <function|version>
+
+Extracts version and function information from the current git tag.
+
+Expects a tag of the format 'v1.2.3/function-name'
+
+EOF
+  exit 1
+}
+
+(($# != 1)) && usage
+
+extract_type="${1}"
+
+function_string="function"
+version_string="version"
+
+# If GIT_TAG is not set, just skip processing.
+if [[ -z "${GIT_TAG+x}" ]];
+then
+  2>&1 echo "Error: GIT_TAG unset"
+  exit 1
+fi
+
+tag_version=$(cut -d'/' -f1 <<< "${GIT_TAG}")
+tag_function=$(cut -d'/' -f2 <<< "${GIT_TAG}")
+
+if [[ "${extract_type}" == "${version_string}" ]]; then
+  echo "${tag_version}"
+  exit 0
+fi
+
+if [[ "${extract_type}" == "${function_string}" ]]; then
+  echo "${tag_function}"
+  exit 0
+fi
+
+2>&1 echo "Error: Invalid extraction type, got ${extract_type}, expected function or version"
+exit 1
